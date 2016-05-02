e1e609744efe1a8fe0fbf2ebce801bb23ba2e078
xPoggi <poggi1@gmx.de>
2016-05-02 18:42:33 +0200

    Create XmlPath.java

---
+++ XmlPath.java
@@ -0,0 +1,161 @@
+package xpathexample;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class XmlPath {
+	private static String xmlFile = "Musikstore.xml";
+	private static String xmlpathFile = "test.xql";
+
+	public static List<String> evalXPath(List<String> xPathExpressions, Document xpathbefehle, XPath xpath) {
+		try {
+			List<String> resultSet = new LinkedList<>();
+			for (String s : xPathExpressions) {
+				if(!s.equals("")){
+					System.out.println(s + " => ");
+					
+					NodeList nodes = (NodeList) xpath.evaluate(s, xpathbefehle, XPathConstants.NODESET);
+	
+					for (int i = 0; i < nodes.getLength(); i++) {
+						StringWriter writer = new StringWriter();
+						Transformer trans = TransformerFactory.newInstance().newTransformer();
+						trans.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+						trans.transform(new DOMSource(nodes.item(i)), new StreamResult(writer));
+						String xml = writer.toString().replace("\t", "").replace("\r\n", "");
+						resultSet.add(xml);
+//						System.out.println("- " + xml);
+					}
+				}
+				else{
+					System.out.println("End of File.");
+				}
+			}
+			return resultSet;
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+			return null;
+		}		
+	}
+	
+	public static void getUserQuery() {
+		
+		try {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Name des Künstlers: ");
+		String name = scan.nextLine();
+
+		System.out.println("Name des Genres: ");
+		String genre = scan.nextLine();
+		
+		if (name.length() > 0 && genre.length() > 0) {
+			
+			String expr = "//titel[kuenstlerID = (//kuenstler[name=\"" + name + "\"]/@kuenstlerID) and @titelID=(//titel_hat_genre[@genre=\"" + genre + "\"]/@titelID)]";
+
+			List<String> list = new LinkedList<>();
+			
+			list.add(expr);
+
+			Document xmlfile = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(xmlFile);
+			XPath xpath = XPathFactory.newInstance().newXPath();
+			
+			List<String> resultXML = evalXPath(list, xmlfile, xpath);
+			
+			//TODO sanitize result
+			resultXML.forEach(result -> System.out.println(result));
+			
+			
+		} else if (name.length()  > 0) {
+			
+			String expr = "//titel[kuenstlerID = (//kuenstler[name=\"" + name + "\"]/@kuenstlerID)]";
+			List<String> list = new LinkedList<>();
+			
+			list.add(expr);
+
+			Document xmlfile = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(xmlFile);
+			XPath xpath = XPathFactory.newInstance().newXPath();
+			
+			List<String> resultXML = evalXPath(list, xmlfile, xpath);
+			
+			//TODO sanitize result
+			resultXML.forEach(result -> System.out.println(result));
+
+			
+		} else if (genre.length() > 0) {
+			
+			String expr = "//titel[@titelID=(//titel_hat_genre[@genre=\"" + genre + "\"]/@titelID)]";
+			List<String> list = new LinkedList<>();
+			
+			list.add(expr);
+
+			Document xmlfile = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(xmlFile);
+			XPath xpath = XPathFactory.newInstance().newXPath();
+			
+			List<String> resultXML = evalXPath(list, xmlfile, xpath);
+			
+			System.out.println(resultXML.size());
+			
+			//TODO sanitize result
+			//TODO get kuenstler
+			
+			resultXML.forEach(result -> System.out.println(result));
+			
+			String res = resultXML.get(0);
+			
+			Matcher m = Pattern.compile("titelID=\"([a-z][0-9]+)\"").matcher(res);
+			if (m.matches()) {
+				System.out.println(m.group(1));
+			}
+
+			
+			//			res.split()
+			
+			
+			
+		}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static void main(String[] args) throws IOException, XPathExpressionException, ParserConfigurationException,
+			SAXException, TransformerFactoryConfigurationError, TransformerException {
+		
+//		List<String> xPathExpressions = Files.readAllLines(Paths.get(xmlpathFile));
+//
+//		Document xpathbefehle = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(xmlFile);
+//		XPath xpath = XPathFactory.newInstance().newXPath();
+//		
+//		evalXPath(xPathExpressions, xpathbefehle, xpath);
+
+		getUserQuery();
+		
+	}
+}
